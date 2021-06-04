b1771bfa21f0d20e9e5048b4e0bbbdceec117d7e
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2021-06-04 14:57:45 +0500

    Init

---
+++ HappyTravel.Suppliers/HappyTravel.Suppliers/Enums/Suppliers.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace HappyTravel.Suppliers.Enums
+{
+    public enum Suppliers
+    {
+        Unknown = 0,
+        Netstorming = 1,
+        
+        [Description("iWTX")] 
+        Illusions = 2,
+        
+        [Description("Direct Contracts")] 
+        DirectContracts = 3,
+        
+        [Description("RateHawk")]
+        Etg = 4,
+        
+        [Description("Rakuten Travel Xchange")]
+        Rakuten = 5,
+        
+        [Description("Columbus (Direct Contracts)")]
+        Columbus = 6,
+        
+        [Description("TravelgateX (TEST)")] 
+        TravelgateXTest = 7,
+        
+        [Description("Jumeirah Hotels")] 
+        Jumeirah = 8,
+        
+        [Description("Darina Holidays")] 
+        Darina = 9
+    }
+}
